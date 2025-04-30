a9e963c6943084fe2fcc42f1f5bd5f700d943f34
Soufiane Boukssim <soufianeboukssim8@gmail.com>
2025-04-30 22:33:04 +0100

    Activation du rechargement dynamique de la configuration avec Spring Cloud Config et Actuator
    - Ajout de `@RefreshScope` sur ConfigTestRestController pour permettre le rechargement à chaud
    - Activation des endpoints Actuator `health` et `refresh` dans application.properties
    - Test réussi de la requête POST /actuator/refresh avec l'en-tête Accept: application/json pour appliquer les modifications de configuration sans redémarrage de l'application.

--- customer-service/src/main/java/com/customerservice/config/ConfigTestRestController.java
+++ customer-service/src/main/java/com/customerservice/config/ConfigTestRestController.java
@@ -2,12 +2,15 @@ package com.customerservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 //1ere manière de configuration
 @RestController
+// On dit a spring que ConfigTestRestController sera instancier a chaque fois qu'on raffrechi la config
+@RefreshScope
 public class ConfigTestRestController {
     @Value("${customer.params.x}")
     private String p1;
