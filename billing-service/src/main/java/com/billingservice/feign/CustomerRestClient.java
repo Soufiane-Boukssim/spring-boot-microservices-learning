b074e82a4e51c721d289556aae35bb2d0a0e7e5e
Soufiane Boukssim <soufianeboukssim8@gmail.com>
2025-05-02 18:37:08 +0100

    🔧 Ajout de Resilience4j Circuit Breaker à CustomerRestClient
    Intégration de Resilience4j Circuit Breaker sur la méthode getCustomerById(Long id) dans l’interface CustomerRestClient.
    
    Cette méthode est utilisée dans BillRestController#getBill(...) pour récupérer les détails du client lors de l'affichage d'une facture.
    
    Objectif : améliorer la résilience du microservice Billing en cas d'indisponibilité temporaire du customer-service.

--- billing-service/src/main/java/com/billingservice/feign/CustomerRestClient.java
+++ billing-service/src/main/java/com/billingservice/feign/CustomerRestClient.java
@@ -1,6 +1,7 @@
 package com.billingservice.feign;
 
 import com.billingservice.models.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface CustomerRestClient {
 
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customerServiceCB", fallbackMethod = "getDefaultCustomer")
     Customer getCustomerById(@PathVariable Long id);
 
     @GetMapping("/customers")
     PagedModel<Customer> getAllCustomers();
+
+    default Customer getDefaultCustomer(Long id, Exception exception) {
+        return Customer.builder()
+                .id(id)
+                .name("Default Name")
+                .email("Default Email")
+                .build();
+    }
 
 }
--- billing-service/src/main/java/com/billingservice/models/Customer.java
+++ billing-service/src/main/java/com/billingservice/models/Customer.java
@@ -1,9 +1,8 @@
 package com.billingservice.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter @Setter
+@Getter @Setter @Builder @AllArgsConstructor @NoArgsConstructor
 public class Customer {
     private Long id;
     private String name;
